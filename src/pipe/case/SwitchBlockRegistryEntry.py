0e7aa69a6b3e563f4567a82727846cc9b9131315
David Gayman <drg.developer.2@gmail.com>
2023-08-19 18:40:09 -0400

    Designing the case block class hierarchy to accomodate all needed capabilities and enforce proper checks.

--- src/pipe/case/SwitchBlockRegistryEntry.py
+++ src/pipe/case/SwitchBlockRegistryEntry.py
@@ -1,15 +1,60 @@
 # AKA switch block entry
 from src.pipe.registry.PipelineRegistryEntry import PipelineRegistryEntry
 
-class SwitchBlockRegistryEntry(PipelineRegistryEntry):
-    def __init__(self, pipe_from=None, pipe_to=None):
-        super().__init__(pipe_from=pipe_from, pipe_to=pipe_to)
-        self.case_block_registry = {}
+class CallableValue:
+    """House a value, or expression used to determine a value."""
 
-class BinarySwitchBlockRegistryEntry(SwitchBlockRegistryEntry):
+    def __init__(self, value_expression):
+
+        self.value_expression_fcn = value_expression
+
+        # Transform a non-callable value into a callable function
+        if not callable(value_expression):
+            self.value_expression = lambda item: value_expression
+
+    def get_value(self, item):
+        return self.value_expression_fcn(item)
+
+
+
+class AbstractSwitchBlockRegistryEntry(PipelineRegistryEntry):
     def __init__(self, pipe_from=None, pipe_to=None):
         super().__init__(pipe_from=pipe_from, pipe_to=pipe_to)
 
-class EnumSwitchBlockRegistryEntry(SwitchBlockRegistryEntry):
+
+class SwitchBlockRegistryEntry(AbstractSwitchBlockRegistryEntry):
+    def __init__(self, match_expression_function, pipe_from=None, pipe_to=None):
+        super().__init__(pipe_from=pipe_from, pipe_to=pipe_to)
+        self.match_expression_function = match_expression_function
+        self.case_block_value_registry = {}
+        self.case_block_expression_registry = {}
+
+    def add_case(self, match_expression, value_expression):
+        if callable(match_expression):
+            self.__add_expression_case(match_expression, value_expression)
+        else:
+            self.__add_value_case(match_expression, value_expression)
+
+    def __add_value_case(self, match_value, value_expression):
+        self.case_block_value_registry[match_value] = CallableValue(value_expression)
+
+    def __add_expression_case(self, match_fcn, value_expression):
+        self.case_block_expression_registry[match_fcn] = CallableValue(value_expression)
+
+
+
+class BinarySwitchBlockRegistryEntry(AbstractSwitchBlockRegistryEntry):
     def __init__(self, pipe_from=None, pipe_to=None):
         super().__init__(pipe_from=pipe_from, pipe_to=pipe_to)
+
+    def add_case(self):
+        if not binary value, throw
+        super.add_case()
+
+class EnumSwitchBlockRegistryEntry(AbstractSwitchBlockRegistryEntry):
+    def __init__(self, pipe_from=None, pipe_to=None):
+        super().__init__(pipe_from=pipe_from, pipe_to=pipe_to)
+
+    def add_case(self):
+        if not enum value, throw
+        super.add_case()
